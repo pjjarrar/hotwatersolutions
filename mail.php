4e793c3cfd4db6df77184a5af302521694dcfbd6
Phillip Jarrar <pjjarrar@gmail.com>
2023-09-23 18:18:20 -0500

    added most of the project files

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $to = "sam@hotwatersolutions.com";
+
+    // $headers = "From: ".$name. "\r\n" .
+    // "CC: contact@BASICLABCONTROLS.COM";
+
+    $headers = "From: ".$name. "\r\n";
+
+    $txt = "You have recieved an e-mail from ".$name ."\r\nEmail: ".$email ."\r\n
+        Message: ". $message;
+
+    if($email!=NULL) {
+        mail($to, $subject, $txt, $headers);
+    }
+
+    header('Location:thankyou.html');
+
+?>
